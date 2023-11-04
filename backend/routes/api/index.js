e8b727d1a073386a3223a65f966f28f8300683c9
tristan <thuckabee.work@gmail.com>
2023-11-04 18:05:07 -0500

    Added User Login

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
-const { requireAuth, restoreUser, setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
+const usersRouter = require('./users.js');
+const sessionRouter = require('./session.js');
+const {
+  requireAuth,
+  restoreUser,
+  setTokenCookie
+} = require('../../utils/auth.js');
 
 router.use(restoreUser);
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 // Add a XSRF-TOKEN cookie
 router.get("/csrf/restore", (req, res) => {
