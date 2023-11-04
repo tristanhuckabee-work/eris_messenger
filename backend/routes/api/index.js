f376f2c7f72550718fbbcdf335221a54568a97b1
tristan <thuckabee.work@gmail.com>
2023-11-04 13:54:24 -0500

    added an API router

---
+++ backend/routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+// Add a XSRF-TOKEN cookie
+router.get("/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({ 'XSRF-Token': csrfToken });
+});
+
+module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
-// Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
-  });
-});
+router.use('/api', apiRouter);
 
 module.exports = router;
